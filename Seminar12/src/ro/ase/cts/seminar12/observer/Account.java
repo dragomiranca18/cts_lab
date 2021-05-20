5829d577f6306cadd58c3108315d0e6f64cc5057
Dragomir Anca-Maria <dragomiranca18@stud.ase.ro>
2021-05-20 08:23:39 +0300

    Seminar 12 break

---
+++ Seminar12/src/ro/ase/cts/seminar12/observer/Account.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.seminar12.observer;
+
+
+
+public abstract class Account {
+
+	//e asbstracta deci nu va putea fi instantiata, va servi drept sablon
+	
+	public abstract double getBalance();
+}
---
+++ Seminar12/src/ro/ase/cts/seminar12/observer/Withdrawable.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.seminar12.observer;
+
+
+
+public interface Withdrawable {
+	public abstract void withdraw(double amount) throws InsufficientFundsException;
+}
