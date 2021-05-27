be13fd6b438c4d2ae7a5ea81ec01495750492ea6
Dragomir Anca-Maria <dragomiranca18@stud.ase.ro>
2021-05-27 09:26:26 +0300

    seminar 13 final

--- Seminar13/src/ro/ase/cts/seminar13/Student.java
+++ Seminar13/src/ro/ase/cts/seminar13/Student.java
@@ -12,6 +12,8 @@ public class Student {
 	String nume;
 	int varsta;
 	int note[];
+	private static final int NOTA_MAXIMA=10;
+	private static final int NOTA_MINIMA=1;
 	
 	public Student(String nume, int varsta, int[] note) {
 		super();
@@ -34,7 +36,7 @@ public class Student {
 
 	public void setVarsta(int varsta) throws StudentExceptionWrongValue{
 		if(varsta<0)
-			throw StudentExceptionWrongValue("Values can't be below 0");
+			throw new StudentExceptionWrongValue("Values can't be below 0");
 		this.varsta = varsta;
 	}
 
@@ -42,9 +44,27 @@ public class Student {
 		return note;
 	}
 
-	public void setNote(int[] note) {
+	public void setNote(int[] note) throws StudentExceptionWrongValue {
+		if(note!=null) {
+		for(int i=0;i<note.length;i++) {
+			if(note[i]>NOTA_MAXIMA || note[i]<NOTA_MINIMA)
+				throw new StudentExceptionWrongValue("Invald input for nota- "+note[i]);
+			}
+		}
 		this.note = note;
 	}
 	
-	
+	public float calculMedie() throws StudentExceptionWrongValue {
+		if(note==null) {
+			throw new StudentExceptionWrongValue("emty dataset - note");
+		}
+		
+		float suma=0;
+		for(int i=0;i<note.length;i++) {
+			suma+=note[i];
+		}
+		
+		float medie=suma /note.length;
+		return (int)(medie*100)/100.0f;
+	}
 }
